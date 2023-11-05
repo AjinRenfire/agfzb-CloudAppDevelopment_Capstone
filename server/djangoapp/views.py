f1b26283a434ce58f6eaba5885efc243ca3c9af1
AjinRenfire <ajinra4@outlook.com>
2023-11-05 08:25:14 -0500

    what am I doing??

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -99,7 +99,15 @@ def get_dealerships(request):
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
-
+def get_dealerships(request):
+    if request.method == "GET":
+        url = "your-cloud-function-domain/dealerships/dealer-get"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
