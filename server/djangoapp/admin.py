ed8ad20b7254045d3d039609ce3f83d8c840424e
AjinRenfire <ajinra4@outlook.com>
2023-11-05 08:02:04 -0500

    rr

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -11,3 +11,14 @@ from django.contrib import admin
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+from .models import CarMake, CarModel
+
+# Register the CarMake model
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')  # Display these fields in the admin list view
+
+# Register the CarModel model
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'get_type_display', 'year')  # Display these fields in the admin list view
